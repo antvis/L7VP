de7ba510072930e261d5e5fd25fa2dbbf4ae5a96
yxh01132861 <yxh01132861@antgroup.com>
2023-07-07 10:42:48 +0800

    fix: 代码优化

--- packages/li-analysis-assets/src/widgets/AnalysisLayout/Component/index.tsx
+++ packages/li-analysis-assets/src/widgets/AnalysisLayout/Component/index.tsx
@@ -1,7 +1,8 @@
 import type { ImplementWidgetProps } from '@antv/li-sdk';
 import { MapContainer } from '@antv/li-sdk';
+import { useSize } from 'ahooks';
 import classNames from 'classnames';
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import FloatPanel from '../FloatPanel';
 import type { Properties } from '../registerForm';
 import { CLS_PREFIX } from './constant';
@@ -17,6 +18,9 @@ const AnalysisLayout: React.FC<AnalysisLayoutProps> = (props) => {
   const { showSidePanel, showBottomPanel, showFloatPanel, collapsedFloatPanel = true, slotsElements, children } = props;
 
   const styles = useStyle();
+
+  const bottomPanelContainerRef = useRef<HTMLDivElement>(null);
+  const size = useSize(bottomPanelContainerRef);
 
   const [collapsed, setCollapsed] = useState(collapsedFloatPanel);
   const mapContainerSlots = useMemo(() => ({ content: slotsElements.content, controls: slotsElements.controls }), [
@@ -34,7 +38,10 @@ const AnalysisLayout: React.FC<AnalysisLayoutProps> = (props) => {
 
   return (
     <div className={classNames(styles.analysisLayout, CLS_PREFIX)}>
-      <div className={styles.layoutContainer} style={showBottomPanel ? { height: 'calc(100% - 300px)' } : {}}>
+      <div
+        className={styles.layoutContainer}
+        style={showBottomPanel ? { height: `calc(100% - ${size?.height}px)` } : {}}
+      >
         <MapContainer
           className={classNames(styles.mapContainer, `${CLS_PREFIX}__map-conatiner`, {
             [styles.showFloatPanel]: showFloatPanel && !collapsed,
@@ -59,7 +66,7 @@ const AnalysisLayout: React.FC<AnalysisLayoutProps> = (props) => {
         )}
       </div>
       {showBottomPanel && (
-        <div className={classNames(styles.bottomPanel, `${CLS_PREFIX}__bottom-panel`)}>
+        <div className={classNames(styles.bottomPanel, `${CLS_PREFIX}__bottom-panel`)} ref={bottomPanelContainerRef}>
           {slotsElements.bottomPanel ? slotsElements.bottomPanel({}) : null}
         </div>
       )}
