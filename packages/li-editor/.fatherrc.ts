3e70f33aa66b4ab6a1ffead5e05deb5a7402efae
lvisei <yunji.me@outlook.com>
2023-07-20 17:24:31 +0800

    build: 优化构建时常 (#12)

--- packages/li-editor/.fatherrc.ts
+++ packages/li-editor/.fatherrc.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'father';
 import { getUMDConfig } from '../../scripts/fatherrc.base';
 
-const isBundle = process.env.BUNDLE_ENV !== 'bundless' && process.env.NODE_ENV === 'production';
+const isBundle = process.env.BUNDLE_ENV === 'bundle' && process.env.NODE_ENV === 'production';
 
 export default defineConfig({
   extends: '../../.fatherrc.base.ts',
