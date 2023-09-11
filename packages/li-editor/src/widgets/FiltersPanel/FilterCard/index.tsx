b0955886be7bf454c4c7b5c1fde7ab1b8e7d2058
yxh01132861 <yxh01132861@antgroup.com>
2023-09-11 15:33:49 +0800

    fix: lint 问题

--- packages/li-editor/src/widgets/FiltersPanel/FilterCard/index.tsx
+++ packages/li-editor/src/widgets/FiltersPanel/FilterCard/index.tsx
@@ -5,7 +5,10 @@ import { getDatasetFields, isLocalOrRemoteDataset } from '@antv/li-sdk';
 import { Button, Card, Popconfirm, Select, theme } from 'antd';
 import type { DefaultOptionType } from 'antd/lib/select';
 import React, { useMemo } from 'react';
-import type { FilterLogicalOperators } from '@antv/li-p2/dist/esm/components/Filter/types';
+import type {
+  FilterLogicalOperators,
+  FilterNode as DefaultFilterNode,
+} from '@antv/li-p2/dist/esm/components/Filter/types';
 import { useEditorDatasets, useEditorState } from '../../../hooks';
 import './index.less';
 
@@ -36,7 +39,7 @@ const FilterCard = ({ id: datasetId, datasetOptions, selectedDatasets, onDataset
 
   const { updateState } = useEditorState();
 
-  const addFilterNode = (filterNode: FilterNode, filterLogicalOperator?: FilterLogicalOperators) => {
+  const addFilterNode = (filterNode: DefaultFilterNode, filterLogicalOperator?: FilterLogicalOperators) => {
     updateState((draft) => {
       const index = draft.datasets.findIndex((item) => item.id === datasetId);
       if (index !== -1) {
