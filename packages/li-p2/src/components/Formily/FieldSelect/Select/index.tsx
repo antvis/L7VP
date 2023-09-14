6e4098bc3547c847abc5d2bd112d040ad6f92cd6
simplexiao <81275881+simplexiao@users.noreply.github.com>
2023-09-14 19:51:49 +0800

    feat: 字段选择组件支持横向滚动 (#24)
    Co-authored-by: yxh01132861 <yxh01132861@antgroup.com>

--- packages/li-p2/src/components/Formily/FieldSelect/Select/index.tsx
+++ packages/li-p2/src/components/Formily/FieldSelect/Select/index.tsx
@@ -1,9 +1,9 @@
 import { usePrefixCls } from '@formily/antd-v5/esm/__builtins__';
 import type { SelectProps } from 'antd';
-import { Select, Tag } from 'antd';
+import { Select, Tag, Empty } from 'antd';
 import cls from 'classnames';
 import { isUndefined } from 'lodash-es';
-import React from 'react';
+import React, { useState } from 'react';
 import useStyle from './style';
 import type { FieldSelectOptionType } from './types';
 
@@ -11,12 +11,55 @@ const InternalSelect: React.FC<SelectProps<string, FieldSelectOptionType>> = (pr
   const { options, ...prop } = props;
   const prefixCls = usePrefixCls('formily-field-select');
   const [wrapSSR, hashId] = useStyle(prefixCls);
+  const [open, setOpen] = useState(false);
 
+  const onOptionClick = (val: string) => {
+    if (props.onChange) {
+      props.onChange(val, options ?? []);
+      setOpen(false);
+    }
+  };
   return wrapSSR(
-    <Select {...prop} popupClassName={cls(`${prefixCls}`, hashId)}>
+    <Select
+      {...prop}
+      popupClassName={cls(`${prefixCls}`, hashId)}
+      open={open}
+      onDropdownVisibleChange={(visible) => setOpen(visible)}
+      dropdownRender={() => {
+        if (!options?.length) {
+          return <Empty description="暂无数据" image={Empty.PRESENTED_IMAGE_SIMPLE} />;
+        }
+
+        const dropdownHeight = 32 * options.length < 256 ? 32 * options.length + 14 : 256;
+        return (
+          <div className={`${prefixCls}-dropdown`} style={{ height: dropdownHeight }}>
+            <div className={`${prefixCls}-dropdown-container`}>
+              {options?.map((item, index) => {
+                return (
+                  <div
+                    className={cls(`${prefixCls}-item`, hashId, {
+                      [`${prefixCls}-item_selected`]: item.value === props.value,
+                    })}
+                    key={index}
+                    onClick={() => onOptionClick(item.value)}
+                  >
+                    {isUndefined(item.type) ? (
+                      <Tag>未知</Tag>
+                    ) : (
+                      <Tag color={item.typeColor}>{isUndefined(item.typeName) ? item.type : item.typeName}</Tag>
+                    )}
+
+                    <span>{item.label}</span>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        );
+      }}
+    >
       {options?.map((item, index) => {
         return (
-          // eslint-disable-next-line react/no-array-index-key
           <Select.Option value={item.value} key={index}>
             <div className={cls(`${prefixCls}-item`, hashId)}>
               {isUndefined(item.type) ? (
@@ -24,7 +67,6 @@ const InternalSelect: React.FC<SelectProps<string, FieldSelectOptionType>> = (pr
               ) : (
                 <Tag color={item.typeColor}>{isUndefined(item.typeName) ? item.type : item.typeName}</Tag>
               )}
-
               <span title={item.label}>{item.label}</span>
             </div>
           </Select.Option>
--- packages/li-p2/src/components/Formily/FieldSelect/Select/style.ts
+++ packages/li-p2/src/components/Formily/FieldSelect/Select/style.ts
@@ -1,12 +1,38 @@
 import { genStyleHook } from '@formily/antd-v5/esm/__builtins__';
 
 export default genStyleHook('field-select', (token) => {
-  const { componentCls } = token;
+  const { componentCls, controlItemBgActive, controlItemBgHover, colorTextDescription } = token;
 
   return {
     [componentCls]: {
+      [`${componentCls}-dropdown`]: {
+        maxHeight: 256,
+        overflow: 'auto',
+        position: 'relative',
+      },
+
+      [`${componentCls}-dropdown-container`]: {
+        position: 'absolute',
+        minWidth: '100%',
+        paddingTop: 7,
+      },
+
       [`${componentCls}-item`]: {
-        lineHeight: '30px',
+        height: '32px',
+        lineHeight: '32px',
+        whiteSpace: 'nowrap',
+        padding: '0 12px',
+        fontSize: '14px',
+        borderRadius: 4,
+        cursor: 'pointer',
+
+        '&:hover': {
+          background: controlItemBgHover,
+        },
+
+        '&_selected': {
+          background: controlItemBgActive,
+        },
       },
     },
   };
