fe818392ce01e0afe9c510209c20a6ec0da2388e
yxh01132861 <yxh01132861@antgroup.com>
2023-09-16 16:14:48 +0800

    fix: 放置位置组件

--- packages/li-p2/src/components/Formily/ControlPositionSelect/Select/index.tsx
+++ packages/li-p2/src/components/Formily/ControlPositionSelect/Select/index.tsx
@@ -1,30 +1,13 @@
 /* eslint-disable react/no-array-index-key */
-import { usePrefixCls } from '@formily/antd-v5/esm/__builtins__';
 import type { SelectProps } from 'antd';
 import { Select } from 'antd';
-import cls from 'classnames';
 import React from 'react';
 import { POSITION } from './contants';
-import useStyle from './style';
 
 const PositionSelect: React.FC<SelectProps> = (props) => {
   const { options, ...prop } = props;
-  const prefixCls = usePrefixCls('formily-position-select');
-  const [wrapSSR, hashId] = useStyle(prefixCls);
 
-  return wrapSSR(
-    <Select {...prop} className={cls(`${prefixCls}-select`, hashId)} popupClassName={cls(`${prefixCls}`, hashId)}>
-      {(options ?? POSITION).map((item, index) => {
-        return (
-          <Select.Option value={item.value} key={index}>
-            <div className={cls(`${prefixCls}-item`, hashId)}>
-              <span>{item.label}</span>
-            </div>
-          </Select.Option>
-        );
-      })}
-    </Select>,
-  );
+  return <Select {...prop} options={options ?? POSITION} />;
 };
 
 export default PositionSelect;
