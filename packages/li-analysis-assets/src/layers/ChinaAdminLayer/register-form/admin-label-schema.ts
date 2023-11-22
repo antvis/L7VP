e5b66047b9a0d9bdc5957c618b03bcfc1ecdabef
yunji <yunji.me@outlook.com>
2023-11-22 17:27:39 +0800

    fix: label stroke 默认值

--- packages/li-analysis-assets/src/layers/ChinaAdminLayer/register-form/admin-label-schema.ts
+++ packages/li-analysis-assets/src/layers/ChinaAdminLayer/register-form/admin-label-schema.ts
@@ -48,7 +48,7 @@ export default () => {
             adminLabelStroke: {
               type: 'string',
               title: '描边颜色',
-              default: '#fff',
+              default: '#606060',
               'x-decorator': 'FormItem',
               'x-component': 'ColorPicker',
               'x-decorator-props': {},
@@ -57,13 +57,14 @@ export default () => {
             adminLabelStrokeWidth: {
               type: 'number',
               title: '描边宽度',
-              default: 1,
+              default: 0.5,
               'x-decorator': 'FormItem',
               'x-component': 'Slider',
               'x-component-props': {
                 placeholder: '描边宽度',
                 min: 0,
                 max: 5,
+                step: 0.5,
               },
               'x-decorator-props': {},
             },
