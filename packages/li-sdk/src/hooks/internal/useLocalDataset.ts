5866498e4f73103c3797432292cccd1b39c52fc1
yunji <yunji.me@outlook.com>
2023-12-15 16:34:01 +0800

    fix: 修改筛选器元数据信息丢失

--- packages/li-sdk/src/hooks/internal/useLocalDataset.ts
+++ packages/li-sdk/src/hooks/internal/useLocalDataset.ts
@@ -4,7 +4,7 @@ import type { DependencyList } from 'react';
 import { useEffect, useMemo, useState } from 'react';
 import type { DatasetFilter, LocalDatasetSchema } from '../../specs';
 import type { LocalDataset } from '../../types';
-import { applyDatasetFilter, filterFalsyDatasetFilter } from '../../utils';
+import { applyDatasetFilter, getValidFilterWithMeta } from '../../utils';
 import { useMemoDeep } from './useMemoDeep';
 
 const customizerDepsEqual = (aDeps: DependencyList = [], bDeps: DependencyList = []) => {
@@ -56,13 +56,7 @@ export function useLocalDataset(datasetSchema: LocalDatasetSchema, pickFilter?: 
     const _filter = pickFilter || datasetSchema.filter;
     if (isUndefined(_filter)) return _filter;
 
-    const filterChildren = filterFalsyDatasetFilter(_filter).children.map((child) => {
-      const column = _columns.find((item) => item.name === child.field);
-      return Object.assign({}, child, column && { fieldMeta: column });
-    });
-    const _filterFormat = { ..._filter, children: filterChildren };
-
-    return _filterFormat;
+    return getValidFilterWithMeta(_filter, _columns);
   }, [datasetSchema.filter, pickFilter, datasetSchema.columns]);
   const params: DatasetServiceParams = useMemoDeep(() => ({ data: datasetSchema.data, filter }), customizerDepsEqual);
 
