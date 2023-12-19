3f2677fc65229738f5efe728b403646492eac7d1
yunji <yunji.me@outlook.com>
2023-12-19 20:14:34 +0800

    chore: zoom style

--- packages/li-core-assets/src/widgets/ZoomControl/ComponenStyle.ts
+++ packages/li-core-assets/src/widgets/ZoomControl/ComponenStyle.ts
@@ -10,61 +10,29 @@ const useStyle = () => {
   return {
     l7Zoom: css`
       .l7-button-control {
-        color: ${colorText};
+        color: ${colorTextDescription};
         background: ${colorBgContainer};
+
+        .l7-iconfont {
+          fill: ${colorTextDescription};
+        }
+      }
+
+      .l7-button-control {
+        border-bottom: 1px solid ${colorBorder};
+      }
+
+      .l7-button-control:hover {
+        background-color: ${colorBgElevated};
 
         .l7-iconfont {
           fill: ${colorText};
         }
       }
 
-      .l7-button-control:first-child {
-        border-bottom: 1px solid ${colorBorder};
+      .l7-control-zoom__number {
+        font-size: 12px;
       }
-
-      .l7-button-control:not(:disabled):hover {
-        background-color: ${colorBgContainer};
-      }
-    `,
-
-    zoomControl: css`
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-    `,
-
-    zoomItem: css`
-      width: 28px;
-      height: 28px;
-      color: ${colorTextDescription};
-      line-height: 28px;
-      text-align: center;
-      vertical-align: middle;
-      background-color: ${colorBgContainer};
-    `,
-
-    zoomBtn: css`
-      font-size: 16px;
-      cursor: pointer;
-
-      &:hover {
-        color: ${colorText};
-        background-color: ${colorBgElevated};
-      }
-    `,
-
-    zoomBtnIn: css`
-      border-radius: 4px 4px 0 0;
-    `,
-
-    zoomBtnOut: css`
-      border-radius: 0 0 4px 4px;
-    `,
-
-    zoomNumber: css`
-      font-size: 12px;
-      border-top: 1px solid ${colorBorder};
-      border-bottom: 1px solid ${colorBorder};
     `,
   };
 };
