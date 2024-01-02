f4e374cc67b8d863881862b03b2e5447a8c9481a
yunji <yunji.me@outlook.com>
2024-01-02 16:35:23 +0800

    fix: 数值筛选器配置态更新问题

--- packages/li-analysis-assets/src/widgets/FilterControl/index.tsx
+++ packages/li-analysis-assets/src/widgets/FilterControl/index.tsx
@@ -6,7 +6,7 @@ export default implementWidget({
   version: 'v0.1',
   metadata: {
     name: 'FilterControl',
-    displayName: '数据筛选控件',
+    displayName: '数据筛选器',
     description: '在地图上的实时筛选数据，支持默认值设置',
     type: 'Auto',
     category: 'DataAnalysis',
