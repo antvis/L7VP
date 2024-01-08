483f46170a332f64897cbc3c242cd3b7252e9128
yxh01132861 <yxh01132861@antgroup.com>
2024-01-08 17:16:18 +0800

    fix: 图标大小问题

--- packages/li-core-assets/src/widgets/FullscreenControl/constants.tsx
+++ packages/li-core-assets/src/widgets/FullscreenControl/constants.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const ICON = () => {
   return (
-    <svg className="l7-iconfont" fill="currentColor" aria-hidden="true">
+    <svg className="l7-iconfont" fill="currentColor" aria-hidden="true" style={{ width: '1em', height: '1em' }}>
       <use href="#l7-icon-fullscreen" />
     </svg>
   );
--- packages/li-core-assets/src/widgets/MapThemeControl/constants.tsx
+++ packages/li-core-assets/src/widgets/MapThemeControl/constants.tsx
@@ -10,7 +10,7 @@ export const POPOVER_PLACEMENT = new Map<string | undefined, string>([
 
 export const ICON = () => {
   return (
-    <svg className="l7-iconfont" fill="currentColor" aria-hidden="true">
+    <svg className="l7-iconfont" fill="currentColor" aria-hidden="true" style={{ width: '1em', height: '1em' }}>
       <use href="#l7-icon-color" />
     </svg>
   );
