b66d999ad8a5d1e0ef8237ba63135465ee5e6af7
yunji <yunji.me@outlook.com>
2024-01-24 13:31:40 +0800

    chore: set dark colorBgBase

--- packages/li-editor/docs/case/Theme/demos/theme.ts
+++ packages/li-editor/docs/case/Theme/demos/theme.ts
@@ -2,6 +2,10 @@ import { theme } from 'antd';
 
 export const ANTD_DEFAULT_THEME = {
   algorithm: theme.defaultAlgorithm,
+  token: {
+    colorPrimary: '#8274FF',
+    colorInfo: '#8274FF',
+  },
 };
 
 export const ANTD_DARK_THEME = {
@@ -9,7 +13,7 @@ export const ANTD_DARK_THEME = {
   token: {
     colorPrimary: '#8274FF',
     colorTextBase: 'rgba(255,255,255,0.85)',
-    colorBgBase: '#0f0f13',
+    colorBgBase: '#1d1e25',
     colorInfo: '#8274FF',
     borderRadius: 6,
   },
