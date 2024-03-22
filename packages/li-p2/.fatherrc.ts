591c270919364c933fa640638c8de007411c42ff
yunji <yunji.me@outlook.com>
2024-03-23 01:20:51 +0800

    chore: npm innore

--- packages/li-p2/.fatherrc.ts
+++ packages/li-p2/.fatherrc.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from 'father';
-import { getUMDConfig } from '../../scripts/fatherrc.base';
-
-const isBundle = process.env.BUNDLE_ENV === 'bundle' && process.env.NODE_ENV === 'production';
 
 export default defineConfig({
   extends: '../../.fatherrc.base.ts',
-  umd: isBundle ? getUMDConfig('LIP2') : undefined,
+  // Non't need UMD
 });
